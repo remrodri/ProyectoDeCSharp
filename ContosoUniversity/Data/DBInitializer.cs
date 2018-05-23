344f4cdcdd421758a7c674a8e89378f4ad716b66
remrodri <rembert.rodrigo@hotmail.com>
2018-05-23 19:26:53 -0400

    inicio del proyecto

---
+++ ContosoUniversity/Data/DBInitializer.cs
@@ -0,0 +1,74 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Linq;
+
+namespace ContosoUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Students.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var students = new Student[]
+            {
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+            new Course{CourseID=1050,Title="Rusia",Credits=3},
+            new Course{CourseID=4022,Title="Alemania",Credits=3},
+            new Course{CourseID=4041,Title="Brasil",Credits=3},
+            new Course{CourseID=1045,Title="Portugal",Credits=4},
+            new Course{CourseID=3141,Title="Argentina",Credits=4},
+            new Course{CourseID=2021,Title="Belgica",Credits=3},
+            new Course{CourseID=2042,Title="Polonia",Credits=4}
+            };
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Group="uno"},
+            new Enrollment{StudentID=1,CourseID=4022,Group="uno"},
+            new Enrollment{StudentID=1,CourseID=4041,Group="uno"},
+            new Enrollment{StudentID=2,CourseID=1045,Group="uno"},
+            new Enrollment{StudentID=2,CourseID=3141,Group="uno"},
+            new Enrollment{StudentID=2,CourseID=2021,Group="uno"},
+            new Enrollment{StudentID=3,CourseID=1050,Group="uno"},
+            new Enrollment{StudentID=4,CourseID=1050,Group="uno"},
+            new Enrollment{StudentID=4,CourseID=4022,Group="uno"},
+            new Enrollment{StudentID=5,CourseID=4041,Group="uno"},
+            new Enrollment{StudentID=6,CourseID=1045,Group="uno"},
+            new Enrollment{StudentID=7,CourseID=3141,Group="uno"}
+            };
+            foreach (Enrollment e in enrollments)
+            {
+                context.Enrollments.Add(e);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿namespace ContosoUniversity.Models
+{
+    //public enum Grade
+    //{
+    //    A, B, C, D
+    //}
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        //public Grade? Group { get; set; }
+        public string Group { get; set; }
+
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
